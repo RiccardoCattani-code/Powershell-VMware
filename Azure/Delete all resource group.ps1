f1c552e88359c4ff35d05045a8ebbebef0dd8250
Riccardo Cattani <riccardocattani.ict@gmail.com>
2025-04-22 21:56:01 +0200

    miglioramento

--- Azure/Delete all resource group.ps1
+++ Azure/Delete all resource group.ps1
@@ -11,22 +11,18 @@ $resourceGroups = Get-AzResourceGroup
 foreach ($rg in $resourceGroups) {
     Write-Host "Deleting resource group:" $rg.ResourceGroupName -ForegroundColor Yellow
     Remove-AzResourceGroup -Name $rg.ResourceGroupName -Force -AsJob
+
+    # Monitor the deletion status for the current resource group
+    do {
+        Start-Sleep -Seconds 10
+        $status = az group show --name $rg.ResourceGroupName --query "properties.provisioningState" -o tsv 2>$null
+        if ($status -eq $null) {
+            Write-Host "Resource group '$($rg.ResourceGroupName)' has been deleted." -ForegroundColor Green
+            break
+        } else {
+            Write-Host "Current status of resource group '$($rg.ResourceGroupName)': $status" -ForegroundColor Cyan
+        }
+    } while ($true)
 }
 
-Write-Host "All resource groups and their contents have been scheduled for deletion." -ForegroundColor Green
-
-# Monitoraggio dello stato tramite Azure CLI
-do {
-    Start-Sleep -Seconds 10
-    $status = az group show --name $rg.ResourceGroupName --query "properties.provisioningState" -o tsv 2>$null
-    if ($status -eq $null) {
-        Write-Host "Il gruppo di risorse '$($rg.ResourceGroupName)' è stato eliminato." -ForegroundColor Green
-        break
-    } else {
-        Write-Host "Stato attuale del gruppo di risorse '$($rg.ResourceGroupName)': $status" -ForegroundColor Cyan
-    }
-} while ($true)
-}
-
-Write-Host "Tutti i gruppi di risorse sono stati elaborati." -ForegroundColor Green
-
+Write-Host "All resource groups have been processed." -ForegroundColor Green
